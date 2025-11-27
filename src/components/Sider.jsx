3ad9b43f8b8ca65162eb1e05ed28e36f3afacd8b
sriharirajaboina <srihari.rajaboina@hexanovamedia.tech>
2025-11-27 10:15:21 +0530

    icon issue completed

--- src/components/Sider.jsx
+++ src/components/Sider.jsx
@@ -8,20 +8,11 @@ import data from "../Data/Data.json";
 import Toggle from "./Toggle";
 import { useTheme } from "../context/ThemeContex";
 
-const menuIcons = import.meta.glob("../assets/menu/*", { eager: true });
-
 const Sider = ({ mobile = false, closeSidebar }) => {
   const { darkMode, setDarkMode } = useTheme();
   const menu = data.menu;
   const insight = data.insight;
-
-
-const getIcon = (fileName) => {
-  const match = Object.entries(menuIcons).find(([key]) =>
-    key.endsWith(fileName)
-  );
-  return match ? match[1].default : "";
-};
+  
 
   return (
     <div
@@ -31,6 +22,7 @@ const getIcon = (fileName) => {
         ${mobile ? "w-full" : "w-60"}
       `}
     >
+      
       {mobile && (
         <div className="flex justify-end mb-4">
           <button onClick={closeSidebar} className="text-3xl text-white">
@@ -38,7 +30,6 @@ const getIcon = (fileName) => {
           </button>
         </div>
       )}
-
       <img src={logo} alt="logo" className="px-2 py-5 w-40 h-auto mb-8" />
 
       <div className="flex ml-3 mb-5 items-center">
@@ -47,7 +38,32 @@ const getIcon = (fileName) => {
       </div>
 
       <div className="mb-10">
-        {menu.map((item) => (
+        {menu.map((item) => {
+          const iconPath = new URL(`../assets/menu/${item.icon}`, import.meta.url).href;
+          return (
+            <Accordion
+              key={item.id}
+              sx={{
+                backgroundColor: "transparent",
+                color: "inherit",
+                boxShadow: "none",
+                width: "100%",
+              }}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+              >
+                <img src={iconPath} className="w-6 h-6 mr-3" />
+                {item.title}
+              </AccordionSummary>
+            </Accordion>
+          );
+        })}
+      </div>
+      <h2 className="text-base font-bold ml-3 mb-3">INSIGHTS</h2>
+      {insight.map((item) => {
+        const iconPath = new URL(`../assets/menu/${item.icon}`, import.meta.url).href;
+        return (
           <Accordion
             key={item.id}
             sx={{
@@ -60,36 +76,12 @@ const getIcon = (fileName) => {
             <AccordionSummary
               expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
             >
-              
-              <img src={getIcon(item.icon)} className="w-6 h-6 mr-3" />
+              <img src={iconPath} className="w-6 h-6 mr-3" />
               {item.title}
             </AccordionSummary>
           </Accordion>
-        ))}
-      </div>
-
-      <h2 className="text-base font-bold ml-3 mb-3">INSIGHTS</h2>
-
-      {insight.map((item) => (
-        <Accordion
-          key={item.id}
-          sx={{
-            backgroundColor: "transparent",
-            color: "inherit",
-            boxShadow: "none",
-            width: "100%",
-          }}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
-          >
-            
-            <img src={getIcon(item.icon)} className="w-6 h-6 mr-3" />
-            {item.title}
-          </AccordionSummary>
-        </Accordion>
-      ))}
-
+        );
+      })}
       <div className="mt-6 ml-3">
         <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
       </div>
@@ -98,7 +90,6 @@ const getIcon = (fileName) => {
 };
 
 export default Sider;
-
 
 
 
